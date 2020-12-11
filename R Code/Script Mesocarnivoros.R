cd7751b717647488e6afed8d5bb8b37c09aa2ce3
Pakillo <f.rodriguez.sanc@gmail.com>
2020-12-11 01:08:20 +0100

    review script

--- R Code/Script Mesocarnivoros.R
+++ R Code/Script Mesocarnivoros.R
@@ -19,17 +19,27 @@ write.bib(citation("activity"))
 write.bib(citation("circular"))
 
 
+### Break up script: prepare_data.R ###
+
 # Import dataset
-raw_data <- read.csv("JPHO_phototraping_definitive.csv", skip = 18, header = T, sep = ";", as.is = T )
+raw_data <- read.csv(here::here("data", "JPHO_phototraping_definitive.csv"),
+                     skip = 18, header = TRUE, sep = ";", as.is = TRUE)
 
 # New variable with species binomial names: genus + species
 
 raw_data$binomial<-paste0(raw_data$genus," ", raw_data$species)
 raw_data$binomial<-as.factor(raw_data$binomial)
-levels(raw_data$binomial)
+levels(raw_data$binomial) ### Check this formally (assertr) ###
 
 # New variable with hms
 raw_data$hms <- as_hms(raw_data$time)
+
+### This generates warning at row 2534:
+raw_data[2534, c("time", "hms")]
+raw_data$time[2534] <- "02:21:05"
+raw_data$hms <- as_hms(raw_data$time)
+###
+
 
 # Package overlap works entirely in radians.
 # New variable: time in radians -> TimeRad
@@ -39,28 +49,47 @@ raw_data$TimeRad <- hms2rad(raw_data$hms)
 # Format change variables: as.factor and as.date
 
 raw_data$area <- as.factor(raw_data$area)
-levels(raw_data$area)
+levels(raw_data$area) ### Check formally (assertr)
 raw_data$ind <- as.factor(raw_data$ind)
 raw_data$sp <- as.factor(raw_data$sp)
 raw_data$behavior <- as.factor(raw_data$behavior)
 raw_data$date <- as.Date(raw_data$date,  "%d/%m/%Y")
 
+### Alternative with dplyr:
+library("dplyr")
+raw_data <- raw_data %>%
+  mutate(across(c(area, ind, sp, behavior), as.factor))
+###
+
+
 # Creation of a new variable and POSIXct format
-?as.POSIXt
+#?as.POSIXt
 raw_data$Date_Time <- paste0(raw_data$date," ", raw_data$time)
 raw_data$Date_Time<- as.POSIXct(raw_data$Date_Time, format = "%Y-%m-%d %H:%M:%S")
 class(raw_data$Date_Time)
 summary(raw_data$Date_Time) #check if dates are comprised between 2018 and 2020
+### check this formally (assertr) ###
 
 # Filter NA values in date and time variables. (remove?)
 # There is setup error in 4 dates: SABOJI O334 cam07 test 4 (delete?)
+### Change data through code, and keep versions! ###
 
 # now that data are cleaned, we call it as meso_data
 
 meso_data <- subset(raw_data, is.na(raw_data$Date_Time)==FALSE)
 
+### Check data
+library("assertr")
+meso_data_check <- meso_data %>%
+  assert(in_set(c("SABCOL", "SABMAR", "SABOJI")), area) %>%
+  verify(between(meso_data$date, as.Date("2018-01-01"), as.Date("2020-12-31"))) %>%
+  assert(in_set(c("Alectoris rufa", "Anthus pratensis")), binomial)
+# note there are typos in species names
+# etc
+###
 
-# As our study is carried out over 2 years, daylight vary according to the seasons 
+
+# As our study is carried out over 2 years, daylight vary according to the seasons
 # This fact could introduce biases. In order to avoid it we perform average anchoring
 
 # solartime() transforms clock time to solar time anchored to sun rise and sunset times for a given location.
@@ -68,7 +97,7 @@ meso_data <- subset(raw_data, is.na(raw_data$Date_Time)==FALSE)
 
 
 
-?solartime
+#?solartime
 solartime <- solartime(meso_data$Date_Time, lat= 37.0000000 , long= -6.3333300,tz= 1  ,"%Y-%m-%d %H:%M:%S")
 solartime # $solar: radian solar time data anchored to average sun rise and sun set times.
 
@@ -83,6 +112,10 @@ class(meso_data$solar_circ)
 write.csv(meso_data, "meso_data.csv")
 write.table(meso_data, "meso_data.xlsx")
 
+
+
+### Break up script: species_detection.R ###
+
 #######################################################################################################################
 #                                                                                                                     #
 #                                    DETECTIONS OF EACH SPECIES PER POPULATION                                        #
@@ -96,11 +129,22 @@ meles <- subset(meso_data, binomial =="Meles meles")
 herpestes <- subset(meso_data, binomial == "Herpestes ichneumon")
 mesocarnivores <- rbind(genetta, vulpes, meles, herpestes)
 
+### dplyr alternative:
+mesocarniv.sp <- c("Genetta genetta", "Vulpes vulpes", "Meles meles", "Herpestes ichneumon")
+mesocarnivores <- meso_data %>%
+  filter(binomial %in% mesocarniv.sp)
+###
+
 # MESOCARNIVORE DETECTIONS PER POPULATION
 summary(genetta$area)
 summary(herpestes$area)
 summary(vulpes$area)
 summary(meles$area)
+
+### dplyr alternative:
+mesocarnivores %>%
+  group_by(binomial, area) %>%
+  summarise(n())
 
 
 #PREYS (SUBSET PER SPECIES):
@@ -111,13 +155,27 @@ mirlo <- subset(meso_data, binomial =="Turdus merula")
 perdiz <- subset(meso_data, binomial =="Alectoris rufa")
 
 
+
 # PREY DETECTIONS PER POPULATION
-summary(rabitt$area)
+summary(rabitt$area) ### typo
 summary(rodent$area)
 summary(zorzal$area)
 summary(mirlo$area)
 summary(perdiz$area)
 
+
+### dplyr alternative:
+preys.sp <- c("Oryctolagus cuniculus", "rodent", "Turdus philomelos",
+              "Turdus merula", "Alectoris rufa")
+preys <- meso_data %>%
+  filter(binomial %in% preys.sp)
+preys %>%
+  group_by(binomial, area) %>%
+  summarise(n())
+
+
+
+### Break up script: ###
 
 ########################################################################################################################
 #                                                                                                                      #
@@ -132,6 +190,11 @@ levels(meso_data$binomial)
 
 activityDensity(recordTable = meso_data, allSpecies = TRUE, speciesCol = "binomial", recordDateTimeCol = "Date_Time",
                 recordDateTimeFormat = "%Y-%m-%d %H:%M:%S", plotR = TRUE)
+### This gives error because Date_Time must be character or factor
+meso_data$Date_Time_chr <- as.character(meso_data$Date_Time)
+activityDensity(recordTable = meso_data, allSpecies = TRUE, speciesCol = "binomial", recordDateTimeCol = "Date_Time_chr",
+                recordDateTimeFormat = "%Y-%m-%d %H:%M:%S", plotR = TRUE)
+###
 
 
 # In order to estimate kernel density plots and get overlapping plots for each pair of sp we use activityOverlap from camtrapR package
@@ -142,6 +205,9 @@ activityDensity(recordTable = meso_data, allSpecies = TRUE, speciesCol = "binomi
 # Meles meles: #9370DB (purple)
 
 ?activityOverlap
+
+
+### Here starts some repeated code ###
 
 # Activity overlaping for mesocarnivores: herpestes ichneumon vs vulpes vulpes. Double anchoring transformated values:
 
@@ -169,6 +235,44 @@ overlapPlot(genetta$solar, vulpes$solar, rug =T,linetype = c(1,1),linecol = c("#
 # Activity overlaping for mesocarnivores: vulpes vulpes vs meles meles
 overlapPlot(vulpes$solar, meles$solar, rug =T,linetype = c(1,1), linecol = c("#4682B4", "#9370DB"),ylim= c(0.00, 0.13),
             olapcol = "#F0F8FF", main="Red fox vs European badger")
+
+
+### Remember the DRY principle (Don't Repeat Yourself). Trying to refactor:
+
+# Write function (and save apart):
+plot_overlap_2spp <- function(sp1, sp2, dataset = meso_data) {
+
+  # Define colors per species
+  color.sp <- data.frame(
+    sp = c("Herpestes ichneumon", "Vulpes vulpes", "Genetta genetta", "Meles meles"),
+    color = c("#F0E68C", "#4682B4", "#66CDAA", "#9370DB")
+  )
+  sp1.color = color.sp$color[color.sp$sp == sp1]
+  sp2.color = color.sp$color[color.sp$sp == sp2]
+
+  # Get times for each species
+  sp1.times <- dataset$solar[dataset$binomial == sp1]
+  sp2.times <- dataset$solar[dataset$binomial == sp2]
+
+  # Plot function
+  overlapPlot(sp1.times, sp2.times,
+              rug = TRUE, linetype = c(1, 1),
+              linecol = c(sp1.color, sp2.color),
+              ylim = c(0.00, 0.13),
+              olapcol = "#F5FFFA",
+              linewidth = c(1, 1),
+              main = paste(sp1, "vs", sp2, sep = " "))
+
+}
+
+
+# Run function:
+plot_overlap_2spp("Vulpes vulpes", "Meles meles")
+plot_overlap_2spp("Genetta genetta", "Vulpes vulpes")
+
+###
+
+
 
 
 #######################################################################################################################
@@ -247,7 +351,7 @@ overlapPlot(perdiz$solar, meles$solar, rug =T,linecol = c("#DAA520", "#9370DB"),
 # Subsets for each sp were extracted previously (see DETECTIONS OF EACH SPECIES PER POPULATION)
 
 # For each sp, we have different number of detections. Each overlap coefficient is related to a determined sample size so Dhat1 will be
-# use when sample size < 50 and Dhat4 when sample size > 50. Thus, we need to check the number of detections for each sp and choose 
+# use when sample size < 50 and Dhat4 when sample size > 50. Thus, we need to check the number of detections for each sp and choose
 # the appropiate coefficient related to the lower number of records from both species.
 
 # ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡  CAUTION !!!!!!!!!!!!!!!!!!!!
@@ -257,9 +361,48 @@ overlapPlot(perdiz$solar, meles$solar, rug =T,linecol = c("#DAA520", "#9370DB"),
 # To get the size of the smaller of the two samples, type:
 
 
+### Repeated code below: try refactoring
+
+# Write function
+calc_overlap <- function(sp.pair, dataset = meso_data) {
+
+  sp1.timerad <- dataset$TimeRad[dataset$binomial == sp.pair[1]]
+  sp2.timerad <- dataset$TimeRad[dataset$binomial == sp.pair[2]]
+
+  # Coding this condition formally below - won't fail (ie. avoid human error)
+  if (min(length(sp1.timerad), length(sp2.timerad)) < 50) {
+    estimator <- "Dhat1"
+  } else {
+    estimator <- "Dhat4"
+  }
+
+  overlap <- overlapEst(sp1.timerad, sp2.timerad, type = estimator)
+  overlap
+
+}
+
+
+## Species pairs to compare:
+sp.pairs <- list(
+  GV = c("Genetta genetta", "Vulpes vulpes"),
+  GH = c("Genetta genetta", "Herpestes ichneumon"),
+  GM = c("Genetta genetta", "Meles meles"),
+  VH = c("Vulpes vulpes", "Herpestes ichneumon")
+  #etc
+)
+
+overlap.pairs <- lapply(sp.pairs, calc_overlap)
+overlap.pairs
+
+
+###
+
+
+
+
 # OVERLAP | Common genet vs Red fox:
 
-min(length(genetta$TimeRad), length(vulpes$TimeRad)) # Sample size < 50 so we should use Dthat 1 (check because new detections could be added to the dataset)
+min(length(genetta$TimeRad), length(vulpes$TimeRad)) # Sample size < 50 so we should use Dthat 1 (check because new detections could be added to the dataset)  ### Problematic
 overlap_GV <- overlapEst(genetta$TimeRad, vulpes$TimeRad)
 overlap_GV
 overlapEst(vulpes$solar, genetta$solar)
@@ -296,14 +439,14 @@ overlapEst(meles$solar, herpestes$solar)
 # In order to estimate confidence interval of the previous coefficients of overlapping, a bootstrap analysis will be performed.
 # Smoothed bootstrap will be used because usual bootsrap method assumes that the sample is fully representative of the populations.
 # Thus, randomly resampling may not estimate well activity patterns. For example, if we take our original records from european badger,
-# which is noctural, strict bootstrapp method will never yield and observation outside that range. 
+# which is noctural, strict bootstrapp method will never yield and observation outside that range.
 
-# OVERLAP PACKAGE:  bootstraps are generated with resample and a smoothing argument can be specified: 
+# OVERLAP PACKAGE:  bootstraps are generated with resample and a smoothing argument can be specified:
 # if smooth = TRUE (the default), smoothed bootstraps are generated.
 
 # IMPORTANT: 10,000 SMOOTHED BOTTSRAPS WILL BE GENERATED
 
-foxboot <- resample(vulpes$solar, 10000) # This produces matrices with a column for each bootstrap sample. 
+foxboot <- resample(vulpes$solar, 10000) # This produces matrices with a column for each bootstrap sample.
 genetboot <- resample(genetta$solar, 10000)
 badgerboot <- resample(meles$solar, 10000)
 mongooseboot<- resample(herpestes$solar, 10000)
@@ -314,7 +457,7 @@ dim(badgerboot)
 dim(mongooseboot)
 
 
-rabbitboot <- resample(rabbit$solar, 10000) # This produces matrices with a column for each bootstrap sample. 
+rabbitboot <- resample(rabbit$solar, 10000) # This produces matrices with a column for each bootstrap sample.
 rodentboot <- resample(rodent$solar, 10000)
 perdizboot <- resample(perdiz$solar, 10000)
 
@@ -385,7 +528,7 @@ CI_RH <- bootCI(overlap_RH[1], boot_RH[,1], conf = 0.99)
 CI_RH
 
 
-# SMALL MAMMAL VS RED FOX 
+# SMALL MAMMAL VS RED FOX
 overlap_SV <- overlapEst(rodent$solar, vulpes$solar)
 overlap_SV # delta 4
 boot_SV<-bootEst(rodentboot, foxboot, adjust=c(NA,1,NA))
@@ -421,7 +564,7 @@ CI_SH
 # PARTRIDGE vs FOX
 
 overlap_PV <- overlapEst(perdiz$solar, vulpes$solar)
-overlap_PV 
+overlap_PV
 boot_PV<-bootEst(perdizboot, foxboot, adjust=c(1,NA,NA))
 boot_PV
 CI_PV <- bootCI(overlap_PV[1], boot_PV[,1], conf = 0.99)
@@ -430,7 +573,7 @@ CI_PV
 # PARTRIDGE vs BADGER
 
 overlap_PM <- overlapEst(perdiz$solar, meles$solar)
-overlap_PM 
+overlap_PM
 boot_PM<-bootEst(perdizboot, badgerboot, adjust=c(1,NA,NA))
 boot_PM
 CI_PM <- bootCI(overlap_PM[1], boot_PM[,1], conf = 0.99)
@@ -439,7 +582,7 @@ CI_PM
 # PARTRIDGE vs GENET
 
 overlap_PG <- overlapEst(perdiz$solar, genetta$solar)
-overlap_PG 
+overlap_PG
 boot_PG<-bootEst(perdizboot, genetboot, adjust=c(1,NA,NA))
 boot_PG
 CI_PG <- bootCI(overlap_PG[1], boot_PG[,1], conf = 0.99)
@@ -449,7 +592,7 @@ CI_PG
 # PARTRIDGE vs MONGOOSE
 
 overlap_PH <- overlapEst(perdiz$solar, herpestes$solar)
-overlap_PH 
+overlap_PH
 boot_PH<-bootEst(perdizboot, mongooseboot, adjust=c(1,NA,NA))
 boot_PH
 CI_PH <- bootCI(overlap_PH[1], boot_PH[,1], conf = 0.99)
@@ -495,13 +638,13 @@ CI_MH <- bootCI(overlap_MH[1], boot_MH[,1], conf = 0.99)
 CI_MH # perc corresponds to the 0.5% and 99.5% percentiles for a 99% confidence interval.
 
 # bootstrap values differ from the estimates because of the bootstrap bias. Therefore the raw percentiles produced by perc
-# need to be adjusted to account for this bias. The appropriate confidence interval is perc – ( mean(BS) – Dhat) which corresponds 
+# need to be adjusted to account for this bias. The appropriate confidence interval is perc – ( mean(BS) – Dhat) which corresponds
 # to basic0 in the bootCI output
 
 # ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡  CAUTION !!!!!!!!!!!!!!!!!!!!
 
 # The coefficient of overlapping takes a value in the interval [0,1]. All the confidence interval estimators except perc involve
-# additive correction which might result in values outside of this range.  
+# additive correction which might result in values outside of this range.
 # bootCIlogit()  can avoid this problem by carrying out the corrections on a logistic scale and back-transforming.
 
 # IF THERE IS UPPER CI VALUES > 1, bootCIlogit should be applied
@@ -531,8 +674,8 @@ logCI_MH <- bootCIlogit(overlap_MH[1], boot_MH[,1], conf = 0.99)
 logCI_MH
 
 
-# The coefficient of overlap is purely descriptive and thus does not provide a threshold value below which two activity patterns 
-# might be significantly different. 
+# The coefficient of overlap is purely descriptive and thus does not provide a threshold value below which two activity patterns
+# might be significantly different.
 
 
 ###### WATSON WHEELER TEST ####### Are the activity patterns statistically different?
@@ -612,7 +755,7 @@ densityPlot(rabbit_col$solar, main= "COLONIZACIÓN",ylim=c(0, 0.13))
 densityPlot(rabbit_oji$solar, main= "OJILLO", ylim=c(0, 0.13))
 densityPlot(rabbit_mar$solar, main= "MARQUÉS", ylim=c(0, 0.13))
 
-watson.wheeler.test(list(rabbit_oji$solar_circ, rabbit_mar$solar_circ))  
+watson.wheeler.test(list(rabbit_oji$solar_circ, rabbit_mar$solar_circ))
 
 
 densityPlot(vulpes_col$solar, main= "COLONIZACIÓN", ylim=c(0, 0.13))
@@ -620,8 +763,8 @@ densityPlot(vulpes_oji$solar, main= "OJILLO", ylim=c(0, 0.13))
 densityPlot(vulpes_mar$solar, main= "MARQUÉS", ylim=c(0, 0.13))
 
 watson.wheeler.test(list(vulpes_col$solar_circ, vulpes_oji$solar_circ))
-watson.wheeler.test(list(vulpes_col$solar_circ, vulpes_mar$solar_circ))                  
-watson.wheeler.test(list(vulpes_oji$solar_circ, vulpes_mar$solar_circ))  
+watson.wheeler.test(list(vulpes_col$solar_circ, vulpes_mar$solar_circ))
+watson.wheeler.test(list(vulpes_oji$solar_circ, vulpes_mar$solar_circ))
 
 
 overlapEst(vulpes_col$solar, rabbit_col$solar) # could not take this one. there is no sufficient detecions for rabbit in SABCOL
@@ -633,16 +776,16 @@ densityPlot(herpestes_oji$solar, main= "OJILLO", ylim=c(0, 0.13))
 densityPlot(herpestes_mar$solar, main= "MARQUÉS", ylim=c(0, 0.13))
 
 watson.wheeler.test(list(herpestes_col$solar_circ, herpestes_oji$solar_circ))
-watson.wheeler.test(list(herpestes_col$solar_circ, herpestes_mar$solar_circ))                  
-watson.wheeler.test(list(herpestes_oji$solar_circ, herpestes_mar$solar_circ))                     
+watson.wheeler.test(list(herpestes_col$solar_circ, herpestes_mar$solar_circ))
+watson.wheeler.test(list(herpestes_oji$solar_circ, herpestes_mar$solar_circ))
 
 overlapEst(vulpes_col$solar, herpestes_col$solar) # could not take this one. there is no sufficient detecions for rabbit in SABCOL
 overlapEst(vulpes_oji$solar, herpestes_oji$solar)
 overlapEst(vulpes_mar$solar, herpestes_mar$solar)
-                    
+
 ## DIEL ACTIVITY PATTERNS: DETECTIONS OF EACH MESOCARNIVORE
 
-# RAO TEST: Our null hypothesis says the data is of a uniform distribution, while the alternate states 
+# RAO TEST: Our null hypothesis says the data is of a uniform distribution, while the alternate states
 # the data shows signs of directionality
 
 
